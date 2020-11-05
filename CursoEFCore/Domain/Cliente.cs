99b099148514b006b1ce7b2b6ab4898c6f498c5c
Leandro Picoli <leandropicoli@live.com>
2020-11-05 18:25:15 -0300

    Criação das classes

---
+++ CursoEFCore/Domain/Cliente.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CursoEFCore.Domain
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Column("Phone")]
+        public string Telefone { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+    }
+}
